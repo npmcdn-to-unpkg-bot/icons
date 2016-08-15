03402605203c4af7895b4ff2293e1b2b3f497b2e
toshafed <krewger@yandex.ru>
2016-08-15 12:49:10 +0300

    firts version with tasks only

---
+++ scripts/list.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+
+const rootPath = process.cwd()
+
+fs.readdir(`${rootPath}/src`, (err, files) => {
+  if (err) {
+    throw err
+  } else {
+    const reg = /^(kb-)(.*)(\.svg)$/i
+    const list = files.map((item) => reg.exec(item)[2])
+
+    fs.writeFile(`${rootPath}/dist/list.json`, JSON.stringify(list), (errWrite) => {
+      if (errWrite) throw err
+    })
+  }
+})
