1dc8ebb7e6caf9db9ca58c4c31cfe7d45279e4c2
Антон Федянин <anton.fedyanin@kupibilet.ru>
2016-08-17 18:04:14 +0300

    refact code

--- scripts/list.js
+++ scripts/list.js
@@ -3,18 +3,12 @@ const fs = require('fs')
 const rootPath = process.cwd()
 
 fs.readdir(`${rootPath}/src`, (err, files) => {
-  if (err) {
-    throw err
-  } else {
-    const reg = /^(kb-)(.*)(\.svg)$/i
-    const list = files.map((item) => reg.exec(item)[2])
+  if (err) throw err
+  const reg = /^(kb-)(.*)(\.svg)$/i
+  const list = files.map((item) => reg.exec(item)[2])
 
-    fs.writeFile(`${rootPath}/dist/sprite.json`, JSON.stringify(list), (errWrite) => {
-      if (errWrite) {
-        throw err
-      } else {
-        console.log('Names list created!')
-      }
-    })
-  }
+  fs.writeFile(`${rootPath}/dist/sprite.json`, JSON.stringify(list), (errWrite) => {
+    if (errWrite) throw err
+    console.log('Names list created!')
+  })
 })
