485c7899049f49a40f041edbff74d4e10759f902
Антон Федянин <anton.fedyanin@kupibilet.ru>
2016-08-18 15:59:19 +0300

    change path for sprite

--- src/scripts/colors.js
+++ src/scripts/colors.js
@@ -2,12 +2,12 @@ const fs = require('fs')
 
 const rootPath = process.cwd()
 
-fs.readFile(`${rootPath}/dist/sprite.svg`, 'utf8', (err, data) => {
+fs.readFile(`${rootPath}/tmp/sprite.svg`, 'utf8', (err, data) => {
   if (err) throw err
 
   const newData = data.replace(/(fill|stroke)="(?!none|transparent).*?"/g, '')
 
-  fs.writeFile(`${rootPath}/dist/sprite.svg`, newData, (errWrite) => {
+  fs.writeFile(`${rootPath}/tmp/sprite.svg`, newData, (errWrite) => {
     if (errWrite) throw err
     console.log('Sprite without colors created!')
   })
