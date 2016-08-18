cf9687d9d353032edc1b316fe3ffdca66fd9ebcc
Anton Fedyanin <anton.fedyanin@kupibilet.ru>
2016-08-18 23:40:28 +0300

    add component

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import pkg from '../package.json'
+import list from '../dist/sprite.json'
+
+const { React, ReactDOM } = window
+
+const Icons = ({ prefix }) =>
+  <div>
+    <h2>version: {pkg.version}</h2>
+    {list.map((item) =>
+      <svg>
+        <use xlinkHref={`#${prefix}-${item}`} key={item} />
+      </svg>
+    )}
+  </div>
+
+Icons.defaultProps = {
+  prefix: 'kb',
+}
+
+Icons.propTypes = {
+  prefix: React.PropTypes.string,
+}
+
+ReactDOM.render(<Icons />, document.getElementById('app'))
