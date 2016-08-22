e8e58a0b7d1c80d5af3596530a44339d84ab81bc
Anton Fedyanin <anton.fedyanin@kupibilet.ru>
2016-08-22 12:22:59 +0300

    change prefix

--- scripts/list.js
+++ scripts/list.js
@@ -6,7 +6,7 @@ const rootPath = process.cwd()
 fs.readdir(`${rootPath}/src/icons/`, (err, files) => {
   if (err) throw err
 
-  const reg = /^(kb-)(.*)(\.svg)$/i
+  const reg = /^(kb_)(.*)(\.svg)$/i
   const list = files.map((item) => reg.exec(item)[2])
 
   mkdirp(`${rootPath}/dist`, (errMkdir) => {
